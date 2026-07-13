529dea81798d4495d03fe40d37f93b43e96fafeb
Federico Pérez Errisuriz <federico.pereze.pe@gmail.com>
2026-07-13 01:48:12 -0600

    feat: Modify the file of RegistroReposity and start work on RegistroService

--- src/Registro/Services/RegistroService.cpp
+++ src/Registro/Services/RegistroService.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+bool RegistroService::validate(const RegistroModel& entity) {
+    if (entity.getIdEquipo() <= 0 && entity.getIdEquipo() != -1) return false;
+    return true;
+}
+
+bool RegistroService::exist(int id) {
+    if (repo.findById(id).getId() == -1) return false;
+    return true;
+}
 
 vector<RegistroModel> RegistroService::findAll() const {
     return repo.findAll();
