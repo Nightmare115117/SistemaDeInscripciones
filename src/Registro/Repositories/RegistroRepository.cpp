db7107da41c59d4621b79728715b633e6231b2fc
Federico Pérez Errisuriz <federico.pereze.pe@gmail.com>
2026-07-12 22:58:44 -0600

    feat: finish class and cpp of registro

--- src/Registro/Repositories/RegistroRepository.cpp
+++ src/Registro/Repositories/RegistroRepository.cpp
@@ -11,7 +11,7 @@ RegistroRepository::RegistroRepository(DBConfig& dbConfig) : dbConfig(dbConfig) 
 vector<RegistroModel> RegistroRepository::findAll() const {
     connection conn(dbConfig.obtenerDatabaseUrl());
     work txn(conn);
-    result r = txn.exec( "SELECT idRegistro, Fecha_hota_registro ,idEquipo FROM Registro");
+    result r = txn.exec("SELECT idRegistro, Fecha_hora_registro ,idEquipo FROM Registro");
     txn.commit();
 
     vector<RegistroModel> lista;
@@ -28,4 +28,43 @@ vector<RegistroModel> RegistroRepository::findAll() const {
 RegistroModel RegistroRepository::findById(int id) const {
     connection conn(dbConfig.obtenerDatabaseUrl());
     work txn(conn);
+    result r = txn.exec("SELECT idRegistro, Fecha_hora_regisro, idEquipo FROM Registro WHERE idRegistro = $1", params{id});
+    txn.commit();
+
+    if (r.empty()){
+        throw runtime_error("Registro con el id: " + to_string(id));
+    }
+
+    RegistroModel registro(r[0]["idEquipo"].as<int>());
+    registro.setId(r[0]["idRegistro"].as<int>());
+    registro.setFecha(r[0]["Fecha_hora_registro"].as<string>());
+
+    return registro;
+}
+
+int RegistroRepository::insert(const RegistroModel& entity) {
+    connection conn(dbConfig.obtenerDatabaseUrl());
+    work txn(conn);
+    result r = txn.exec("INSERT INTO Registro (idEquipo) VALUES ($1) RETURNING idRegistro", params{
+        entity.getIdEquipo() //parametro 1
+    });
+
+    txn.commit();
+    return r.affected_rows();
+}
+
+bool RegistroRepository::update(const RegistroModel& entity) {
+    throw logic_error("No se permite actualizar un registro una vez creado");
+}
+
+bool RegistroRepository::remove(int id) {
+    connection conn(dbConfig.obtenerDatabaseUrl());
+    work txn(conn);
+    result r = txn.exec("DELETE FROM Registro WHERE idREgistro = $1", 
+        params{
+            id //parametro 1
+        });
+    txn.commit();
+
+    return r.affected_rows() > 0;
 }
